8bbf6ccd37d5759ed9dcf5f96cb5b90f4904d794
Dwiky Erlangga <dwiky.e26@gmail.com>
2017-12-01 02:51:13 +0000

    Add cors

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 var express = require("express"),
     app = express(),
     port = process.env.PORT || 3000,
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    cors = require('cors');
+
     
 var todoRoutes = require('./routes/todos');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/views'));
